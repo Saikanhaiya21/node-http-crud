9a9f3b6a090e547d3cd5218ba71397645fcc5695
saikanhaiya <saikanhaiya36@gmail.com>
2025-09-13 12:26:15 +0530

    Add default route

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const http = require('http');
 const PORT = 3000;
 const HOSTNAME = 'localhost';
 
-http.createServer((req, res) => {
+const index = (req, res) =>{
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end('Crud Operation\n');
+    res.end('Crud Operation!\n');
+}
+
+http.createServer((req, res) => {
+    if(req.url === '/'){
+        return index(req,res);
+
+        res.writeHead(404);
+        res.end(http.STATUS_CODES[404])
+    }
 }).listen(PORT, HOSTNAME, () => {
     console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
 });
